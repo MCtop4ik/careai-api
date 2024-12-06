aa9b5ebe341e0e57d0c17b410be8342fb4ca57ba
Zetni <azakharov@workmovr.com>
2024-12-06 20:38:55 +0400

    fix migrations

--- db/migrate/20241205050515_tech_problems.rb
+++ db/migrate/20241205050515_tech_problems.rb
@@ -1,4 +1,4 @@
-class TechProblems < ActiveRecord::Migration[6.1]
+class CreateTechProblems < ActiveRecord::Migration[6.1]
   def change
     create_table :tech_problems do |t|
       t.string :userid
--- db/migrate/20241206123903_add_diets_column.rb
+++
@@ -1,4 +0,0 @@
-class AddDietsColumn < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
